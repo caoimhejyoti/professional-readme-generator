422498967138bd930ef66d7e60102de59cf7fe9b
caoimhejyoti <caoimhejyoti@gmail.com>
2023-01-19 17:43:03 +0800

    func link resolved + fnc operational + q updates

--- Develop/index.js
+++ Develop/index.js
@@ -1,18 +1,16 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-// const generate = generateMarkdown(/utils/generateMarkdown.js);
+const generateMarkdown = require(`./utils/generateMarkdown.js`);
 // TODO: Create an array of questions for user input
 const questions = [
     {type: "input",
     message: "What is the Title of this project?",
     name: "title",
-    required: true,
     },
     {type: "input",
     message: "Describe the project?",
     name: "description",
-    required: true,
     },
     // FIXME: this needs to be populated based on the questions answered, not a user inputed element. Table of contents is a list of links for each section of the file.
     //{type: "input",
@@ -22,7 +20,6 @@ const questions = [
     {type: "input",
     message: "What are the installation instructions?",
     name: "installation",
-    required: true,
     },
     {type: "input",
     message: "Do you have any usage information?",
@@ -72,28 +69,30 @@ const questions = [
     {type: "input",
     message: "What is your GitHub username?",
     name: "username",
-    required: true,
     },
     // FIXME: this information will create a link to the users email address within questions
     {type: "input",
     message: "What is your email address?",
     name: "email",
-    required: true,
     },
 ];
 
 
 // // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-inquirer
-    .prompt(questions)
-        .then ((data) => {
-            fs.appendFile(`${data.title}-README.md`, generateMarkdowns, (err) =>
-            err ? console.error(err) : console.log('ReadMe created!')
-)});
-
+// function writeToFile(fileName, data) {
+function writeToFile() {
+    inquirer
+        .prompt(questions)
+            .then ((data) => {
+                fs.appendFile(`${data.title}-README.md`, generateMarkdown(data), (err) =>
+                err ? console.error(err) : console.log('ReadMe appended!')
+                )
+            });
+};
 // // TODO: Create a function to initialize app
-// function init() {}
+function init() {
+    writeToFile();
+}
 
 // // Function call to initialize app
-// init();
+init();
