ba7ee7fc239212b07d6dcbd01582c94dfc1d3fea
caoimhejyoti <caoimhejyoti@gmail.com>
2023-01-20 14:51:25 +0800

    updatring license fnc to include new options

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,13 +1,4 @@
-// DESCRIPTION: Function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-// function renderLicenseBadgeFnc(license) {
-//   if (!license){
-//     return "";
-//   }else{
-//     return `${license}`;
-//   }
-// };
-
+// DESCRIPTION: Function that returns a license badge based on user license choice.
 function renderLicenseBadgeFnc(license) {
   switch (license) {
     case 'Apache license 2.0':
@@ -16,23 +7,32 @@ function renderLicenseBadgeFnc(license) {
     case 'BSD 2-clause "Simplified" license':
       badge = `![BSD 2-clause Simplified](https://img.shields.io/badge/License-BSD_2--clause_Simplified-purple?style=for-the-badge)`;
       break;
+    case 'BSD 3-clause "New" or "Revised: license':
+      badge = `![BSD 3-clause](https://img.shields.io/badge/License-BSD_3--clause_clear-purple?style=for-the-badge)`;
+      break;
+    case 'Common Development and Distribution License 1.0':
+      badge = `![CDDL-1.0](https://img.shields.io/badge/License-Common_Development_and_Distribution_1.0-purple?style=for-the-badge)`;
+      break;
+    case 'Eclipse Public License 2.0':
+      badge = `![Eclipse Public License 2.0](https://img.shields.io/badge/License-Eclipse_Public_2.0-purple?style=for-the-badge)`;
+      break;
+    case 'GNU General Public License v2.0':
+      badge = `![GNU General Public License v2.0](https://img.shields.io/badge/License-GNU_General_Public_v2.0-purple?style=for-the-badge)`;
+      break;
+    case 'GNU General Public License v3.0':
+      badge = `![GNU General Public License v3.0](https://img.shields.io/badge/License-GNU_General_Public_v3.0-purple?style=for-the-badge)`;
+      break;
+    case 'MIT':
+      badge = `![MIT](https://img.shields.io/badge/License-MIT-purple?style=for-the-badge)`;
+      break;
+    case 'Mozilla Public License 2.0':
+      badge = `![Mozilla Public License 2.0](https://img.shields.io/badge/License-Mozilla_Public_2.0-purple?style=for-the-badge)`;
+      break;      
   }
   return badge;
-
 }
 
-
-// FIXME: open source URL needs updating. DESCRIPTION: Function that returns the license link
-// If there is no license, return an empty string
-// function renderLicenseLinkFnc(url) {
-//   if (!url) {
-//     return "";
-//   }else{
-//     //FIXME: Confirm this is correct
-//     return `For further information regarding the license, please follow the link below: \n${url}`;
-//   }
-// };
-
+// DESCRIPTION: Function that returns the license link based on user license choice.
 function renderLicenseLinkFnc(license) {
   switch (license) {
     case 'Apache license 2.0':
@@ -41,9 +41,29 @@ function renderLicenseLinkFnc(license) {
     case 'BSD 2-clause "Simplified" license':
       link = `https://opensource.org/licenses/BSD-2-Clause`;
       break;
+      case 'BSD 3-clause "New" or "Revised: license':
+      link = `https://opensource.org/licenses/BSD-3-Clause`;
+      break;
+    case 'Common Development and Distribution License 1.0':
+      link = `https://opensource.org/licenses/CDDL-1.0`;
+      break;
+    case 'Eclipse Public License 2.0':
+      link = `https://opensource.org/licenses/EPL-2.0`;
+      break;
+    case 'GNU General Public License v2.0':
+      link = `https://opensource.org/licenses/GPL-2.0`;
+      break;
+    case 'GNU General Public License v3.0':
+      link = `https://opensource.org/licenses/GPL-3.0`;
+      break;
+    case 'MIT':
+      link = `https://opensource.org/licenses/MIT`;
+      break;
+    case 'Mozilla Public License 2.0':
+      link = `https://opensource.org/licenses/MPL-2.0`;
+      break;
   }
   return link;
-
 }
 
 
