26e1787f6b08cbf67208bad431901da4f63e0fab
caoimhejyoti <caoimhejyoti@gmail.com>
2023-01-20 14:38:00 +0800

    trialing license bugs with switch fix

--- Develop/index.js
+++ Develop/index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 //DESCRIPTION: links to generateMarkdown js file for exported packages.
 const generateMarkdown = require(`./utils/generateMarkdown.js`);
 
-// FIXME: add styling. DESCRIPTION: Array of questions for user input
+// FIXME: add styling. confirm number of licenses required. DESCRIPTION: Array of questions for user input
 const questions = [
     //DESCRIPTION: Generates contents for README title
     {name: "title",
@@ -32,119 +32,133 @@ const questions = [
     type: "list",
     message: "Select your lisence type?",
     choices: [
-        {
-            name:"Academic Free License v3.0",
-            value: "![Academic Free v3.0](https://img.shields.io/badge/License-Academic_Free_v3.0-purple?style=for-the-badge)"
-        }, 
-        {
-            name:"Apache license 2.0",
-            value: "![Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-purple?style=for-the-badge)"
-        }, 
-        {
-            name:"Artistic license 2.0",
-            value:"![Artistic 2.0](https://img.shields.io/badge/License-Artistic_2.0-purple?style=for-the-badge)" 
-        },
-        {
-            name:"Boost Software License 1.0",
-            value:"![Boost Software 1.0](https://img.shields.io/badge/License-boost_software_1.0-purple?style=for-the-badge)"
-        },
-        {
-            name:"BSD 2-clause license",
-            value:"![BSD 2-clause Simplified](https://img.shields.io/badge/License-BSD_2--clause_Simplified-purple?style=for-the-badge)"
-        },
-        {
-            name:"BSD 3-clause license",
-            value:"![BSD 3-clause Clear](https://img.shields.io/badge/License-BSD_3--clause_clear-purple?style=for-the-badge)"
-        },
-        {
-            name:"Creative Commons license", 
-            value:"![Creative Commons](https://img.shields.io/badge/License-Creative_Commons-purple?style=for-the-badge) "
-        },
-        {
-            name:"Do What The F*ck You Want To Public License", 
-            value: "![Do What The F*ck You Want To Public License](https://img.shields.io/badge/License-Do_What_The_F*ck_You_Want_To_Public-purple?style=for-the-badge)"
-        },
-        {
-            name:"Educational Community License v2.0", 
-            value:"![Eclipse Public License 1.0](https://img.shields.io/badge/License--purple?style=for-the-badge)"
-        },
-        {
-            name:"Eclipse Public License 1.0", 
-            value:"![](https://img.shields.io/badge/License-Eclipse_Public_1.0-purple?style=for-the-badge)"
-        },
-        {
-            name:"Eclipse Public License 2.0", 
-            value:"![Eclipse Public License 2.0](https://img.shields.io/badge/License-Eclipse_Public_2.0-purple?style=for-the-badge)"
-        },
-        {
-            name:"European Union Public License 1.1", 
-            value:"![European Union Public License 1.1](https://img.shields.io/badge/License-European_Union_Public_1.1-purple?style=for-the-badge)"
-        },
-        {
-            name:"GNU Affero General Public License v3.0", 
-            value:"![GNU Affero General Public License v3.0](https://img.shields.io/badge/License-GNU_Affero_General_Public_v3.0-purple?style=for-the-badge)"
-        },
-        {
-            name:"GNU General Public License v2.0", 
-            value:"![GNU General Public License v2.0](https://img.shields.io/badge/License-GNU_General_Public_v2.0-purple?style=for-the-badge)"
-        },
-        {
-            name:"GNU General Public License v3.0", 
-            value:"![GNU General Public License v3.0](https://img.shields.io/badge/License-GNU_General_Public_v3.0-purple?style=for-the-badge)"
-        },
-        {
-            name:"GNU Lesser General Public License v2.1", 
-            value:"![GNU Lesser General Public License v2.1](https://img.shields.io/badge/License-GNU_Lesser_General_Public_v2.1-purple?style=for-the-badge)"
-        },
-        {
-            name:"GNU Lesser General Public License v3.0", 
-            value:"![GNU Lesser General Public License v3.0](https://img.shields.io/badge/License-GNU_Lesser_General_Public_v3.0-purple?style=for-the-badge)"
-        },
-        {
-            name:"ISC", 
-            value:"![ISC](https://img.shields.io/badge/License-ISC-purple?style=for-the-badge)"
-        },
-        {
-            name:"LaTeX Project Public License v1.3c", 
-            value:"![LaTeX Project Public License v1.3c](https://img.shields.io/badge/License-LaTeX_Project_Public_v1.3c-purple?style=for-the-badge)"
-        },
-        {
-            name:"Microsoft Public License", 
-            value:"![Microsoft Public License](https://img.shields.io/badge/License-Microsoft_Public-purple?style=for-the-badge)"
-        },
-        {
-            name:"MIT",
-            value:"![MIT](https://img.shields.io/badge/License-MIT-purple?style=for-the-badge)"
-        },
-        {
-            name:"Mozilla Public License 2.0", 
-            value:"![Mozilla Public License 2.0](https://img.shields.io/badge/License-Mozilla_Public_2.0-purple?style=for-the-badge)"
-        },
-        {
-            name:"Open Software License 3.0", 
-            value:"![Open Software License 3.0](https://img.shields.io/badge/License-Open_Software_3.0-purple?style=for-the-badge)"
-        },
-        {
-            name:"PostgreSQL License", 
-            value:"![PostgreSQL License](https://img.shields.io/badge/License-PostgreSQL-purple?style=for-the-badge)"
-        },
-        {
-            name:"SIL Open Font License 1.1", 
-            value:"![SIL Open Font License 1.1](https://img.shields.io/badge/License-SIL_Open_Font_1.1-purple?style=for-the-badge)"
-        },
-        {
-            name:"University of Illinois/NCSA Open Source License", 
-            value:"![University of Illinois/NCSA Open Source License](https://img.shields.io/badge/License-University_of_Illinois/NCSA_Open_Source-purple?style=for-the-badge)"
-        },
-        {
-            name:"The Unlicense", 
-            value:"![The Unlicense](https://img.shields.io/badge/License-The_Unlicense-purple?style=for-the-badge)"
-        },
-        {
-            name:"zLib License", 
-            value:"![zLib License](https://img.shields.io/badge/License-zLib-purple?style=for-the-badge)"
-        }
-    ]},
+        'Apache license 2.0',
+        'BSD 2-clause "Simplified" license',
+        'BSD 3-clause "New" or "Revised" license',
+        'Common Development and Distribution License 1.0',
+    ]
+    },
+    // choices: [
+    //     // {
+    //     //     name:"Academic Free License v3.0",
+    //     //     value: "![Academic Free v3.0](https://img.shields.io/badge/License-Academic_Free_v3.0-purple?style=for-the-badge)"
+    //     // }, 
+    //     {
+    //         name:"Apache license 2.0",
+    //         value: `[![Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-purple?style=for-the-badge)](https://opensource.org/licenses/Apache-2.0)`,
+    //         // value: `<a href ="https://opensource.org/licenses/Apache-2.0"><img alt="license repo is published under with link to open source information." src="![Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-purple?style=for-the-badge)" target="blank/></a>`,
+    //         // url: "https://opensource.org/licenses/Apache-2.0"
+
+    //     }, 
+    //     // {
+    //     //     name:"Artistic license 2.0",
+    //     //     value:"![Artistic 2.0](https://img.shields.io/badge/License-Artistic_2.0-purple?style=for-the-badge)" 
+    //     // },
+    //     // {
+    //     //     name:"Boost Software License 1.0",
+    //     //     value:"![Boost Software 1.0](https://img.shields.io/badge/License-boost_software_1.0-purple?style=for-the-badge)"
+    //     // },
+    //     {
+    //         name:"BSD 2-clause license",
+    //         value:"![BSD 2-clause Simplified](https://img.shields.io/badge/License-BSD_2--clause_Simplified-purple?style=for-the-badge)"
+    //     },
+    //     {
+    //         name:"BSD 3-clause license",
+    //         value:"![BSD 3-clause](https://img.shields.io/badge/License-BSD_3--clause_clear-purple?style=for-the-badge)"
+    //     },
+    //     {
+    //         name: "Common Development and Distribution License 1.0",
+    //         value: "![CDDL-1.0](https://img.shields.io/badge/License-Common_Development_and_Distribution_1.0-purple?style=for-the-badge)",
+    //     },
+    //     // {
+    //     //     name:"Creative Commons license", 
+    //     //     value:"![Creative Commons](https://img.shields.io/badge/License-Creative_Commons-purple?style=for-the-badge) "
+    //     // },
+    //     // {
+    //     //     name:"Do What The F*ck You Want To Public License", 
+    //     //     value: "![Do What The F*ck You Want To Public License](https://img.shields.io/badge/License-Do_What_The_F*ck_You_Want_To_Public-purple?style=for-the-badge)"
+    //     // },
+    //     // {
+    //     //     name:"Educational Community License v2.0", 
+    //     //     value:"![Eclipse Public License 1.0](https://img.shields.io/badge/License--purple?style=for-the-badge)"
+    //     // },
+    //     // {
+    //     //     name:"Eclipse Public License 1.0", 
+    //     //     value:"![](https://img.shields.io/badge/License-Eclipse_Public_1.0-purple?style=for-the-badge)"
+    //     // },
+    //     {
+    //         name:"Eclipse Public License 2.0", 
+    //         value:"![Eclipse Public License 2.0](https://img.shields.io/badge/License-Eclipse_Public_2.0-purple?style=for-the-badge)"
+    //     },
+    //     // {
+    //     //     name:"European Union Public License 1.1", 
+    //     //     value:"![European Union Public License 1.1](https://img.shields.io/badge/License-European_Union_Public_1.1-purple?style=for-the-badge)"
+    //     // },
+    //     // {
+    //     //     name:"GNU Affero General Public License v3.0", 
+    //     //     value:"![GNU Affero General Public License v3.0](https://img.shields.io/badge/License-GNU_Affero_General_Public_v3.0-purple?style=for-the-badge)"
+    //     // },
+    //     {
+    //         name:"GNU General Public License v2.0", 
+    //         value:"![GNU General Public License v2.0](https://img.shields.io/badge/License-GNU_General_Public_v2.0-purple?style=for-the-badge)"
+    //     },
+    //     {
+    //         name:"GNU General Public License v3.0", 
+    //         value:"![GNU General Public License v3.0](https://img.shields.io/badge/License-GNU_General_Public_v3.0-purple?style=for-the-badge)"
+    //     },
+    //     // {
+    //     //     name:"GNU Lesser General Public License v2.1", 
+    //     //     value:"![GNU Lesser General Public License v2.1](https://img.shields.io/badge/License-GNU_Lesser_General_Public_v2.1-purple?style=for-the-badge)"
+    //     // },
+    //     // {
+    //     //     name:"GNU Lesser General Public License v3.0", 
+    //     //     value:"![GNU Lesser General Public License v3.0](https://img.shields.io/badge/License-GNU_Lesser_General_Public_v3.0-purple?style=for-the-badge)"
+    //     // },
+    //     // {
+    //     //     name:"ISC", 
+    //     //     value:"![ISC](https://img.shields.io/badge/License-ISC-purple?style=for-the-badge)"
+    //     // },
+    //     // {
+    //     //     name:"LaTeX Project Public License v1.3c", 
+    //     //     value:"![LaTeX Project Public License v1.3c](https://img.shields.io/badge/License-LaTeX_Project_Public_v1.3c-purple?style=for-the-badge)"
+    //     // },
+    //     // {
+    //     //     name:"Microsoft Public License", 
+    //     //     value:"![Microsoft Public License](https://img.shields.io/badge/License-Microsoft_Public-purple?style=for-the-badge)"
+    //     // },
+    //     {
+    //         name:"MIT",
+    //         value:"![MIT](https://img.shields.io/badge/License-MIT-purple?style=for-the-badge)"
+    //     },
+    //     {
+    //         name:"Mozilla Public License 2.0", 
+    //         value:"![Mozilla Public License 2.0](https://img.shields.io/badge/License-Mozilla_Public_2.0-purple?style=for-the-badge)"
+    //     },
+    //     // {
+    //     //     name:"Open Software License 3.0", 
+    //     //     value:"![Open Software License 3.0](https://img.shields.io/badge/License-Open_Software_3.0-purple?style=for-the-badge)"
+    //     // },
+    //     // {
+    //     //     name:"PostgreSQL License", 
+    //     //     value:"![PostgreSQL License](https://img.shields.io/badge/License-PostgreSQL-purple?style=for-the-badge)"
+    //     // },
+    //     // {
+    //     //     name:"SIL Open Font License 1.1", 
+    //     //     value:"![SIL Open Font License 1.1](https://img.shields.io/badge/License-SIL_Open_Font_1.1-purple?style=for-the-badge)"
+    //     // },
+    //     // {
+    //     //     name:"University of Illinois/NCSA Open Source License", 
+    //     //     value:"![University of Illinois/NCSA Open Source License](https://img.shields.io/badge/License-University_of_Illinois/NCSA_Open_Source-purple?style=for-the-badge)"
+    //     // },
+    //     // {
+    //     //     name:"The Unlicense", 
+    //     //     value:"![The Unlicense](https://img.shields.io/badge/License-The_Unlicense-purple?style=for-the-badge)"
+    //     // },
+    //     // {
+    //     //     name:"zLib License", 
+    //     //     value:"![zLib License](https://img.shields.io/badge/License-zLib-purple?style=for-the-badge)"
+    //     // }
+    // ]},
     //DESCRIPTION: Generates contents for README contribution instructions
     {name: "contributing",
     type: "input",
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,23 +1,51 @@
 // DESCRIPTION: Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+// function renderLicenseBadgeFnc(license) {
+//   if (!license){
+//     return "";
+//   }else{
+//     return `${license}`;
+//   }
+// };
+
 function renderLicenseBadgeFnc(license) {
-  if (!license){
-    return "";
-  }else{
-    return `${license}`;
+  switch (license) {
+    case 'Apache license 2.0':
+      badge = `[![Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-purple?style=for-the-badge)](https://opensource.org/licenses/Apache-2.0)`;
+      break;
+    case 'BSD 2-clause "Simplified" license':
+      badge = `![BSD 2-clause Simplified](https://img.shields.io/badge/License-BSD_2--clause_Simplified-purple?style=for-the-badge)`;
+      break;
   }
-};
+  return badge;
+
+}
+
 
 // FIXME: open source URL needs updating. DESCRIPTION: Function that returns the license link
 // If there is no license, return an empty string
+// function renderLicenseLinkFnc(url) {
+//   if (!url) {
+//     return "";
+//   }else{
+//     //FIXME: Confirm this is correct
+//     return `For further information regarding the license, please follow the link below: \n${url}`;
+//   }
+// };
+
 function renderLicenseLinkFnc(license) {
-  if (!license) {
-    return "";
-  }else{
-    //FIXME: Confirm this is correct
-    return `For further information regarding the license, please follow the link below: \n(https://opensource.org/licenses/${license})`;
+  switch (license) {
+    case 'Apache license 2.0':
+      link = `https://opensource.org/licenses/Apache-2.0`;
+      break;
+    case 'BSD 2-clause "Simplified" license':
+      link = `https://opensource.org/licenses/BSD-2-Clause`;
+      break;
   }
-};
+  return link;
+
+}
+
 
 //DESCRIPTION: Function that returns the license section of README
 // If there is no license, return an empty string
@@ -25,7 +53,7 @@ function renderLicenseSectionFnc(license) {
   if (!license) {
     return "";
   }else{
-    return `This project is using the following license:\n${license}`;
+    return `This project is using the following license:\n**${license}**\n\nFor further information regarding the license, please follow the link below:\n ${renderLicenseLinkFnc(license)}`;
   }
 };
 
@@ -38,35 +66,47 @@ ${renderLicenseBadgeFnc(data.license)}
 ## Description
 ${data.description}
 
-FIXME: Confirm hyperlinks in contents work.
+FIXME: Confirm hyperlinks in contents work - check with Jasmine/Marco regarding links. working in md file not preview?
 ## Table of Contents
-[Installation](#Installation)
-[Usage](#Usage)
-[License](#License)
-[Contribution](#Contribution)
-[Tests](#Tests)
-[Questions](#Questions)
+[Installation](##Installation)
+[Usage](##Usage)
+[License](##License)
+[Contribution](##Contribution)
+[Tests](##Tests)
+[Questions](##Questions)
 
-## Installation
+## Installation   
 ${data.installation}
 
-## Usage
+[Return to top](#${data.title})
+
+## Usage 
 ${data.usage}
+
+[Return to top](#${data.title})
 
 ## License
 ${renderLicenseSectionFnc(data.license)}
-${renderLicenseLinkFnc(data.license)}
+
+[Return to top](#${data.title})
 
 ## Contribution 
 ${data.contributing}
 
+[Return to top](#${data.title})
+
 ## Tests
 ${data.tests}
 
-## Questions
+[Return to top](#${data.title})
+
+## Questions 
 If you have any further questions, please contact me via email or github.
+
 <a href="mailto:${data.email}"><img alt="Link to email contact address" src="https://img.shields.io/badge/email-D14836?style=for-the-badge" target="_blank" /></a>  <a href="https://github.com/${data.username}"><img alt="badge for GitHub" src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white" target="_blank" /></a>
-`;
+
+
+[Return to top](#${data.title})`;
 };
 
 // DESCRIPTION: exporting functions for use within index.js
