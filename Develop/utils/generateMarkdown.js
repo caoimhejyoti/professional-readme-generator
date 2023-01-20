c8aedfbf3f8ca961a39d11953599246b9d55b9bb
caoimhejyoti <caoimhejyoti@gmail.com>
2023-01-20 13:12:41 +0800

    fixing license name display + email badge + layout

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -15,62 +15,58 @@ function renderLicenseLinkFnc(license) {
     return "";
   }else{
     //FIXME: Confirm this is correct
-    return `(https://opensource.org/licenses/${license})`;
+    return `For further information regarding the license, please follow the link below: \n(https://opensource.org/licenses/${license})`;
   }
 };
 
-// FIXME: Confirm Lisence name is working correctly. DESCRIPTION: Function that returns the license section of README
+//DESCRIPTION: Function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSectionFnc(license) {
   if (!license) {
     return "";
   }else{
-    return `This project is using the ${license} license`;
+    return `This project is using the following license:\n${license}`;
   }
 };
 
-// FIXME: Confirm hyperlinks in contents work. Email badge is just GMAIL currently. DESCRIPTION: Function to generate markdown for README
+// FIXME: Confirm hyperlinks in contents work. DESCRIPTION: Function to generate markdown for README
 function generateMarkdown(data) {
-  return `
-  # ${data.title}
-  ${renderLicenseBadgeFnc(data.license)}
+return `
+# ${data.title}
+${renderLicenseBadgeFnc(data.license)}
 
-  ## Description
-  ${data.description}
+## Description
+${data.description}
 
-  FIXME: Confirm hyperlinks in contents work.
-  ## Table of Contents
-  [Installation](#Installation)
-  [Usage](#Usage)
-  [License](#License)
-  [Contribution](#Contribution)
-  [Tests](#Tests)
-  [Questions](#Questions)
+FIXME: Confirm hyperlinks in contents work.
+## Table of Contents
+[Installation](#Installation)
+[Usage](#Usage)
+[License](#License)
+[Contribution](#Contribution)
+[Tests](#Tests)
+[Questions](#Questions)
 
-  ## Installation
-  ${data.installation}
-  
-  ## Usage
-  ${data.usage}
-  
-  ## License
-  ${renderLicenseSectionFnc(data.license)}
-  ${renderLicenseLinkFnc(data.license)}
-  
-  ## Contribution 
-  ${data.contributing}
-  
-  ## Tests
-  ${data.tests}
-  
-  ## Questions
-  If you have any further questions, please contact me via email or github.
+## Installation
+${data.installation}
 
-  //FIXME: this is currently only gmail badge - not inclusive. 
-  <a href="mailto:${data.email}"><img alt="Link to email contact address" src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white"/></a>  <a href="https://github.com/${data.username}"><img alt="badge for GitHub" src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white" target="_blank" /></a>
-  
+## Usage
+${data.usage}
 
-  `;
+## License
+${renderLicenseSectionFnc(data.license)}
+${renderLicenseLinkFnc(data.license)}
+
+## Contribution 
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any further questions, please contact me via email or github.
+<a href="mailto:${data.email}"><img alt="Link to email contact address" src="https://img.shields.io/badge/email-D14836?style=for-the-badge" target="_blank" /></a>  <a href="https://github.com/${data.username}"><img alt="badge for GitHub" src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white" target="_blank" /></a>
+`;
 };
 
 // DESCRIPTION: exporting functions for use within index.js
